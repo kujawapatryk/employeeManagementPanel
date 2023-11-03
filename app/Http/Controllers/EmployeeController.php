0f5b34629e175a1384176789a812d4908416e382
kujawapatryk <homik363@wp.pl>
2023-11-04 00:23:26 +0100

    Creating a controller for actions related to employees.

---
+++ app/Http/Controllers/EmployeeController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class EmployeeController extends Controller
+{
+    public function all()
+    {
+
+    }
+
+    public function showDetails()
+    {
+
+    }
+
+    public function add()
+    {
+
+    }
+
+    public function edit()
+    {
+
+    }
+
+    public function remove()
+    {
+
+    }
+    //
+}
