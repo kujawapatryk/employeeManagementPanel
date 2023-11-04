5541696620fae7ac6b741965efaca04d81c5beb6
kujawapatryk <homik363@wp.pl>
2023-11-04 20:16:35 +0100

    Created the main layout of the page.

---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="pl">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Nawigacja</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+
+<body class="bg-gray-100">
+
+<div class="flex flex-col items-center justify-center h-screen">
+    <nav class="bg-blue-500 p-4 mx-auto max-w-screen-lg rounded mb-8">
+        <ul class="flex space-x-4">
+            <li class="nav-item active">
+                <a class="text-white px-4 py-2 rounded-full" href="/">Wszyscy Pracownicy</a>
+            </li>
+            <li class="nav-item">
+                <a class="text-white px-4 py-2 rounded-full" href="{{route('employees.create')}}">Dodaj pracownika</a>
+            </li>
+            <li class="nav-item">
+                <a class="text-white px-4 py-2 rounded-full" href="#">Firmy</a>
+            </li>
+            <li class="nav-item">
+                <a class="text-white px-4 py-2 rounded-full" href="#">Preferencje żywieniowe</a>
+            </li>
+        </ul>
+    </nav>
+
+    <div class="container mx-auto p-6 bg-white shadow-lg rounded-lg max-w-screen-lg">
+
+        <!-- Tutaj możesz umieścić zawartość dla poszczególnych akcji -->
+
+        @yield('content')
+
+        <!-- Tutaj możesz umieścić zawartość dla poszczególnych akcji -->
+
+
+    </div>
+</div>
+
+</body>
+
+</html>
