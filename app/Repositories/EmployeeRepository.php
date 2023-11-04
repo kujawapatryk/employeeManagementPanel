83ccf962f0e7ff3cfcdc08abdfbe1cf1a9b8c801
kujawapatryk <homik363@wp.pl>
2023-11-04 20:12:34 +0100

    Created a repository for actions related to employees.

---
+++ app/Repositories/EmployeeRepository.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\Employee;
+
+class EmployeeRepository
+{
+    private Employee $employee;
+
+    public function __construct(Employee $employee){
+        $this->employee = $employee;
+    }
+
+    public function getOne(int $id)
+    {
+        return $this->employee->find($id);
+    }
+
+    public function filterBy()
+    {
+        return $this->employee->get();
+    }
+}
