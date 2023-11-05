f9c0727c7a70277050e8f14ab41a20d5906ef44d
kujawapatryk <homik363@wp.pl>
2023-11-05 02:56:54 +0100

    Added a flash message view, where in case of an error, the form data is not reset.

--- resources/views/employee/create.blade.php
+++ resources/views/employee/create.blade.php
@@ -1,8 +1,8 @@
 @extends('layouts.main')
 
 @section('content')
-
-    <form method="POST" action="{{ route('employees.create') }}" class="w-full max-w-lg">
+    @include('components.alerts')
+    <form method="POST" action="{{ route('employees.store') }}" class="w-full max-w-lg">
         @csrf
 
         <div class="flex flex-wrap -mx-3 mb-6">
@@ -10,13 +10,13 @@
                 <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="first-name">
                     Imię
                 </label>
-                <input class="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="first-name" type="text" name="first_name" required>
+                <input class="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="first-name" type="text" name="first_name" value="{{ old('first_name') }}" required>
             </div>
             <div class="w-full md:w-1/2 px-3">
                 <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="last-name">
                     Nazwisko
                 </label>
-                <input class="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white" id="last-name" type="text" name="last_name" required>
+                <input class="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white" id="last-name" type="text" name="last_name" value="{{ old('last_name') }}" required>
             </div>
         </div>
 
@@ -28,9 +28,20 @@
                 <select class="block w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="company" name="company_id" required>
                     <option value="">Wybierz firmę</option>
                     @foreach($companies as $company)
-                        <option value="{{ $company->id }}">{{ $company->name }}</option>
+                        <option value="{{ $company->id }}" {{ (old('company_id') == $company->id) ? 'selected' : '' }}>
+                            {{ $company->name }}
+                        </option>
                     @endforeach
                 </select>
+            </div>
+        </div>
+
+        <div class="flex flex-wrap -mx-3 mb-6">
+            <div class="w-full px-3">
+                <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="email">
+                    E-mail
+                </label>
+                <input class="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="email" type="text" name="email" placeholder="Dodaj adres e-mail" value="{{ old('email') }}" required>
             </div>
         </div>
 
@@ -52,7 +63,10 @@
                 <select class="block w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="dietary-preferences" name="dietary_preference_id" required>
                     <option value="">Wybierz preferencje</option>
                     @foreach($dietaryPreferences as $preference)
-                        <option value="{{ $preference->id }}">{{ $preference->name }}</option>
+                        <option value="{{ $preference->id }}" {{ (old('dietary_preference_id') == $preference->id) ? 'selected' : '' }}>
+                            {{ $preference->name }}
+                        </option>
+{{--                        <option value="{{ $preference->id }}">{{ $preference->name }}</option>--}}
                     @endforeach
                 </select>
             </div>
