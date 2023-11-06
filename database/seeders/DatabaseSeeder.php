9afe4450dc5a7d7aedcfd6dd5808e449f90cc228
kujawapatryk <homik363@wp.pl>
2023-11-06 21:38:01 +0100

    Add custom seeders to DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,6 +12,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        $this->call([
+            CompaniesTableSeeder::class,
+            DietaryPreferencesTableSeeder::class,
+            EmployeesTableSeeder::class
+        ]);
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
