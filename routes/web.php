8fc4f7cbef897a5589e6d62e92b95569cfd15d0c
kujawapatryk <homik363@wp.pl>
2023-11-06 15:34:24 +0100

    Set the starting address to /employees

--- routes/web.php
+++ routes/web.php
@@ -17,9 +17,7 @@ use Illuminate\Support\Facades\Route;
 
 
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::redirect('/', '/employees');
 
 Route::group([
     'prefix' => 'employees',
