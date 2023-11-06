b3278319988be077a6300940cc3f0adfb4ab65e4
kujawapatryk <homik363@wp.pl>
2023-11-06 05:15:11 +0100

    Commented out inactive navigation elements.

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -10,35 +10,29 @@
 
 <body class="bg-gray-100">
 
-<div class="flex flex-col items-center justify-center h-screen">
-    <nav class="bg-blue-500 p-4 mx-auto max-w-screen-xl rounded mb-8">
-        <ul class="flex space-x-4">
-            <li class="nav-item active">
-                <a class="text-white px-4 py-2 rounded-full" href="{{route('employees.list')}}">Wszyscy Pracownicy</a>
-            </li>
-            <li class="nav-item">
-                <a class="text-white px-4 py-2 rounded-full" href="{{route('employees.create')}}">Dodaj pracownika</a>
-            </li>
-            <li class="nav-item">
-                <a class="text-white px-4 py-2 rounded-full" href="#">Firmy</a>
-            </li>
-            <li class="nav-item">
-                <a class="text-white px-4 py-2 rounded-full" href="#">Preferencje żywieniowe</a>
-            </li>
-        </ul>
-    </nav>
+    <div class="flex flex-col items-center justify-center min-h-screen ">
+        <nav class="bg-blue-500 p-4 mx-auto max-w-screen-xl rounded mb-8">
+            <ul class="flex space-x-4">
+                <li class="nav-item active">
+                    <a class="text-white px-4 py-2 rounded-full" href="{{route('employees.list')}}">Wszyscy Pracownicy</a>
+                </li>
+                <li class="nav-item">
+                    <a class="text-white px-4 py-2 rounded-full" href="{{route('employees.create')}}">Dodaj pracownika</a>
+                </li>
+{{--                <li class="nav-item">--}}
+{{--                    <a class="text-white px-4 py-2 rounded-full" href="#">Firmy</a>--}}
+{{--                </li>--}}
+{{--                <li class="nav-item">--}}
+{{--                    <a class="text-white px-4 py-2 rounded-full" href="#">Preferencje żywieniowe</a>--}}
+{{--                </li>--}}
+            </ul>
+        </nav>
 
-    <div class="container mx-auto p-6 bg-white shadow-lg rounded-lg max-w-screen-xl">
-        @include('components.alerts')
-        <!-- Tutaj możesz umieścić zawartość dla poszczególnych akcji -->
-
-        @yield('content')
-
-        <!-- Tutaj możesz umieścić zawartość dla poszczególnych akcji -->
-
-
+        <div class="container mx-auto p-6 bg-white shadow-lg rounded-lg max-w-screen-xl">
+            @include('components.alerts')
+            @yield('content')
+        </div>
     </div>
-</div>
 
 </body>
 
